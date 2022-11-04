6f0c17ab83108b7aaf66aad3f1f51a16ce03dd2a
Aimee <aimeeray3@gmail.com>
2022-11-04 12:53:01 -0600

    week 3 assesement

---
+++ about.js
@@ -0,0 +1,20 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	alert('form has been submitted succesfully');
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+ function mousearr(mouseover){
+	
+ }
+addEventListener('mouseover', (event) => {
+
+})
---
+++ profile.js
@@ -0,0 +1,29 @@
+//create event listeners that check for a click on each of these buttons.
+
+//If a user clicks on “My Favorite Color”, it should alert the user of your favorite color, and so on.
+
+
+
+
+
+
+
+
+
+	
+ 
+
+
+function handleSubmit(evt , myfavoritecolor) {
+	evt.preventDefault();
+	let evt = (blue)
+}
+    
+    alert('blue');
+    addEventListener('mouseover',(evt) => {})
+    
+
+
+
+
+
